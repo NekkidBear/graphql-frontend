cf5e533b4f9ab3df75284965636fff8541416b89
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-25 12:54:04 -0500

    create environment variable for backend URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import React from "react";
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  from,
-} from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import TransactionBuckets from "./components/TransactionBuckets";
-import Customers from "./components/Customers";
-import Accounts from "./components/Accounts";
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import CustomerAccountSummary from './components/CustomerSummary';
 
 // GraphQL endpoint
 const httpLink = new HttpLink({
-  uri: "https://mason-tanaka-c0244r0vl-nekkidbears-projects.vercel.app/graphql",
-  credentials: 'include', // This is important for CORS
-});
-
-// Error handling
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.forEach(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  uri: process.env.REACT_APP_GRAPHQL_URL || 'https://mason-tanaka-c0244r0vl-nekkidbears-projects.vercel.app/graphql',
+  credentials: 'include' // This is important for CORS with credentials
 });
 
 const client = new ApolloClient({
-  link: from([errorLink, httpLink]),
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
@@ -38,9 +18,7 @@ const App: React.FC = () => {
     <ApolloProvider client={client}>
       <div className="App">
         <h1>Customer Account Summary</h1>
-        <Customers />
-        <TransactionBuckets />
-        <Accounts />
+        <CustomerAccountSummary username="nelsonmaria" />
       </div>
     </ApolloProvider>
   );
