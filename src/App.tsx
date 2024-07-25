6f9f39610bf9055691eda443a7738f7294c1b5e2
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-25 13:01:50 -0500

    Add components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
-import CustomerAccountSummary from './components/CustomerSummary';
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+import CustomerAccountSummary from "./components/CustomerSummary";
+import Accounts from "./components/Accounts";
+import Customers from "./components/Customers";
+import TransactionBuckets from "./components/TransactionBuckets";
 
 // GraphQL endpoint
 const httpLink = new HttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_URL || 'https://mason-tanaka-c0244r0vl-nekkidbears-projects.vercel.app/graphql',
-  credentials: 'include' // This is important for CORS with credentials
+  uri:
+    process.env.REACT_APP_GRAPHQL_URL ||
+    "https://mason-tanaka-c0244r0vl-nekkidbears-projects.vercel.app/graphql",
+  credentials: "include", // This is important for CORS with credentials
 });
 
 const client = new ApolloClient({
@@ -19,6 +29,9 @@ const App: React.FC = () => {
       <div className="App">
         <h1>Customer Account Summary</h1>
         <CustomerAccountSummary username="nelsonmaria" />
+        <TransactionBuckets />
+        <Accounts />
+        <Customers />
       </div>
     </ApolloProvider>
   );
